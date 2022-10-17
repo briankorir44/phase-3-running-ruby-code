174002bc488fe6a06642e4c0e2883ba5726f5da1
korir <briankorir44@gmai.com>
2022-10-17 14:51:18 +0300

    complete assignment

---
+++ app.rb
@@ -0,0 +1,11 @@
+# # app.rb
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# app.rb
+# puts [1, 2, 3]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
